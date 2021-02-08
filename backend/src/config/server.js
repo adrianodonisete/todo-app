a7201097da0a77934cc88ce5314f19815b6db5f0
adrianodonisete <adrianodonisete@gmail.com>
2021-02-07 23:28:11 -0300

    server configuration

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,5 +1,3 @@
-const port = 3003;
-
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
@@ -9,6 +7,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
 
+const port = 3003;
 server.listen(port, function () {
 	console.log(`BACKEND is running on port ${port}.`);
 });
